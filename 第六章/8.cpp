e307d4a9aa28e7124a402e685451a9a7029ca6fb
Htto Hu <huyuantao@outlook.com>
2019-11-24 18:00:33 +0800

    .

---
+++ 第六章/8.cpp
@@ -0,0 +1,31 @@
+﻿#include <cstdio>
+#include <cstring>
+#include <queue>
+
+struct Node {
+	int v[3], dist;
+	bool operator<(const Node& rhs)const {
+		return dist > rhs.dist;
+	}
+};
+const int maxn = 205;
+int vis[maxn][maxn], cap[3], ans[maxn];
+void update_ans(const Node& u) {
+	for (int i = 0; i < 3; i++)
+	{
+		int d = u.v[i];
+		if (ans[d] < 0 || u.dist < ans[d])
+			ans[d] = u.dist;
+	}
+}
+
+void solve(int a, int b,int c, int d)
+{
+	cap[0] = a; cap[1] = b; cap[2] = c;
+	memset(vis, 0, sizeof(vis));
+	memset(ans, -1, sizeof(ans));
+	std::priority_queue<Node> q;
+	Node start;
+	start.dist = 0;
+	start.v[0] = 0;
+}
