7bd6048252f74a1e108be064323ed1c498e2427e
Htto Hu <huyuantao@outlook.com>
2019-12-18 23:26:50 +0800

    好像和答案不投额。。。也没找出为啥

---
+++ 第六章/paintBall.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+/*
+3
+500 500 499
+0 0 999
+1000 1000 200
+*/
+using namespace std;
+const int maxn=1000;
+int warface[maxn+1][maxn+1];
+void paint(int a,int b,int c){
+    // hight->height.
+    int max_hight=b+c;
+    int min_hight=b-c;
+    int max_width=a+c;
+    int min_width=a-c;
+    if(max_hight>maxn){max_hight=maxn;}
+    if(min_hight<0){min_hight=0;}
+    if(max_width>maxn){max_width=maxn;}
+    if(min_width<0){min_width=0;}
+    for(int i=min_width;i<=max_width;i++)
+    {
+        for(int j=min_hight;j<=max_hight;j++){
+            if(sqrt((a-i)*(a-i)+(b-j)*(b-j))<=c){
+                warface[i][j]=-1;
+            }
+        }
+    }
+}
+void dfs(int x,int y){
+    if(x<0||x>maxn||y<0||y>maxn) return;
+    if(!warface[x][y]){
+        warface[x][y]=1;
+    }
+    else return;
+    dfs(x+1,y);
+    dfs(x,y+1);
+    dfs(x+1,y+1);
+}
+void print_war_face()
+{
+    for(int i=0;i<=6;i++){
+        for(int j=0;j<=6;j++){
+            std::cout<<warface[i][j];
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    memset(warface,0,sizeof(warface));
+    int n;
+    cin>>n;
+    while(n--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        paint(a,b,c);
+    }
+    int x,y,ok=0;
+    for(int i=maxn;i>=0;i--){
+        if(warface[0][i])
+            continue;
+        dfs(0,i);
+        for(int j=maxn;j>=0;j--){
+            if(warface[maxn][j]==1){
+                x=i;y=j;
+                ok=1;
+                break;
+            }
+        }
+        if(ok)break;
+    }
+    if(ok){
+        cout<<"POSSIBLE: "<<x<<" "<<y;
+    }
+    else
+    {
+        cout<<"IMPOSSIBLE";
+    }
+    return 0;
+    
+}
